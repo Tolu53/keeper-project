e32d8b71a080b023f3563af39c8652e443dd1c00
Tolu53 <115922354+Tolu53@users.noreply.github.com>
2023-01-28 08:42:30 -0500

    New keeper App

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="bg-red-300 w-full h-screen">
+        <h1> hey</h1>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
